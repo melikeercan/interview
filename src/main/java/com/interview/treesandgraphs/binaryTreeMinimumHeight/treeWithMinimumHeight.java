f385a4b0b63cebc8eea1c43e2c1f26d49cb8d929
Melike <mercan@netas.com.tr>
2017-06-19 19:58:21 +0300

    #reasons min height tree

--- src/main/java/com/interview/treesandgraphs/binaryTreeMinimumHeight/treeWithMinimumHeight.java
+++ src/main/java/com/interview/treesandgraphs/binaryTreeMinimumHeight/treeWithMinimumHeight.java
@@ -7,18 +7,36 @@ import com.interview.treesandgraphs.balanced.TreeNode;
 
 public class treeWithMinimumHeight {
 
-    private static int rootSize = 0;
+    private static int rootSize = 1;
+
+    private static int[] arrs2 = new int[11];
 
     public static void main(String[] args) {
-        TreeNode root = new TreeNode(0);
+
+        for (int i = 0; i < arrs2.length; i++) {
+            arrs2[i] = i;
+        }
+        TreeNode root = new TreeNode(arrs2[0]);
         TreeNode parent = root;
-        while (rootSize < 10) {
+        while (rootSize < arrs2.length) {
             System.out.println(rootSize);
-            addNode(parent, parent.getLeft(), parent.getRight(), 10);
+            addNode(parent, parent.getLeft(), parent.getRight(), arrs2.length);
         }
         System.out.println(root);
 
         printTreeBreathFirst(root);
+
+        System.out.println("-------");
+
+        int[] arrs = new int[11];
+        for (int i = 0; i < arrs.length; i++) {
+            arrs[i] = i;
+        }
+
+        TreeNode result = addToTree(arrs, 0, arrs.length - 1);
+        System.out.println(result);
+        printTreeBreathFirst(result);
+
     }
 
     private static void printTreeBreathFirst(TreeNode root) {
@@ -41,10 +59,10 @@ public class treeWithMinimumHeight {
     private static void addNode(TreeNode parent, TreeNode left, TreeNode right, int limit) {
         if (left == null) {
             rootSize++;
-            parent.setLeft(new TreeNode(rootSize));
+            parent.setLeft(new TreeNode(arrs2[rootSize - 1]));
         } else if (right == null) {
             rootSize++;
-            parent.setRight(new TreeNode(rootSize));
+            parent.setRight(new TreeNode(arrs2[rootSize - 1]));
         } else {
             TreeNode leftLeft = left.getLeft();
             TreeNode leftRight = left.getRight();
@@ -58,6 +76,17 @@ public class treeWithMinimumHeight {
             }
         }
 
+    }
+
+    private static TreeNode addToTree(int[] arr, int start, int end) {
+        if (end < start) {
+            return null;
+        }
+        int mid = (start + end) / 2;
+        TreeNode n = new TreeNode(arr[mid]);
+        n.setLeft(addToTree(arr, start, mid - 1));
+        n.setRight(addToTree(arr, mid + 1, end));
+        return n;
     }
 
     public static int minimumDepth(TreeNode node) {
---
+++ src/main/java/com/interview/treesandgraphs/depthList/question.md
@@ -0,0 +1,4 @@
+Trees and Graphs
+
+4.4 Given a binary search tree, design an algorithm which creates a linked list of all the
+nodes at each depth (i.e., if you have a tree with depth D, you’ll have D linked lists).
